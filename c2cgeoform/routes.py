b47cf1ebef8e0174a0cd33fbedca460a3615cc99
Stéphane Brunner <stephane.brunner@camptocamp.com>
2018-02-24 16:05:47 +0100

    Add route prefix

--- c2cgeoform/routes.py
+++ c2cgeoform/routes.py
@@ -7,7 +7,7 @@ def table_pregenerator(request, elements, kw):
     return elements, kw
 
 
-def register_models(config, models):
+def register_models(config, models, prefix=''):
 
     def rec_with_pregenerator(route, pattern):
         config.add_route(route, pattern, pregenerator=table_pregenerator)
@@ -23,14 +23,12 @@ def register_models(config, models):
         tables.append(table)
 
     table_regex = '|'.join(['({})'.format(table['key']) for table in tables])
-    base_route = '/{{table:{}}}'.format(table_regex)
+    base_route = '{}/{{table:{}}}'.format(prefix, table_regex)
 
-    rec_with_pregenerator('c2cgeoform_index', '{}'.format(base_route))
+    rec_with_pregenerator('c2cgeoform_index', base_route)
     rec_with_pregenerator('c2cgeoform_grid', '{}/grid.json'.format(base_route))
-    rec_with_pregenerator('c2cgeoform_item', '{}/{{id}}'
-                          .format(base_route))
-    rec_with_pregenerator('c2cgeoform_item_duplicate', '{}/{{id}}/duplicate'
-                          .format(base_route))
+    rec_with_pregenerator('c2cgeoform_item', '{}/{{id}}'.format(base_route))
+    rec_with_pregenerator('c2cgeoform_item_duplicate', '{}/{{id}}/duplicate'.format(base_route))
 
     def add_global(event):
         event['tables'] = tables
