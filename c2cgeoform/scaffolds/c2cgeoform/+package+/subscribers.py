b9b39c0975266b7805b88679fee0136996d776d5
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2018-03-07 13:51:28 +0100

    Fix translation system

--- c2cgeoform/scaffolds/c2cgeoform/+package+/subscribers.py
+++ c2cgeoform/scaffolds/c2cgeoform/+package+/subscribers.py
@@ -1,56 +1,28 @@
-from pyramid.i18n import get_localizer, TranslationStringFactory
-from pyramid.threadlocal import get_current_request
-from pyramid.threadlocal import get_current_registry
 from pyramid.events import subscriber, BeforeRender, NewRequest
+from pyramid.i18n import get_localizer, TranslationStringFactory
 
-
-_LOCALE_ = '_LOCALE_'
+# see https://docs.pylonsproject.org/projects/pyramid-cookbook/en/latest/templates/mako_i18n.html*
 
 
 @subscriber(BeforeRender)
 def add_renderer_globals(event):
-    request = event.get('request')
-    if request is None:
-        request = get_current_request()
-    if request is not None:
-        # make `_` available in Chameleon templates, so that you can
-        # use e.g. `${_(u"Error")}` in templates
-        event['_'] = request.translate
-        event['localizer'] = request.localizer
+    request = event['request']
+    event['_'] = request.translate
+    event['localizer'] = request.localizer
 
 
-tsf = TranslationStringFactory('c2cgeoform')
+tsf1 = TranslationStringFactory('{{package}}')
+tsf2 = TranslationStringFactory('c2cgeoform')
 
 
 @subscriber(NewRequest)
 def add_localizer(event):
-    _set_accepted_languages_locale(event)
     request = event.request
     localizer = get_localizer(request)
 
-    def auto_translate(string):
-        return localizer.translate(tsf(string))
+    def auto_translate(*args, **kwargs):
+        result = localizer.translate(tsf1(*args, **kwargs))
+        return localizer.translate(tsf2(*args, **kwargs)) if result == args[0] else result
 
     request.localizer = localizer
     request.translate = auto_translate
-
-
-def _set_accepted_languages_locale(event):
-    """ Set the language depending on the browser settings.
-    """
-    if getattr(event.request, _LOCALE_, None) is not None or \
-       event.request.params.get(_LOCALE_) is not None or \
-       event.request.cookies.get(_LOCALE_) is not None:
-        # _LOCALE_ was explicitly set, do not change
-        return
-
-    # _LOCALE_ is not set, try to get the preferred language
-    if not event.request.accept_language:
-        return
-    accepted = event.request.accept_language
-
-    settings = get_current_registry().settings
-    languages = settings['pyramid.available_languages'].split()
-    default_language = settings['pyramid.default_locale_name']
-
-    event.request._LOCALE_ = accepted.best_match(languages, default_language)
