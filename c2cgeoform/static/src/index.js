7c9c92fd3eb363b1e2dcaf7ba45bee4086a5d6d6
Antoine Abt <antoine@abt.im>
2019-10-07 15:12:55 +0200

    feat(proj4): add proj4 & enable 21781

--- c2cgeoform/static/src/index.js
+++ c2cgeoform/static/src/index.js
@@ -3,9 +3,21 @@ import GeoJSONFormat from 'ol/format/GeoJSON'
 import Map from 'ol/Map'
 import VectorSource from 'ol/source/Vector'
 import View from 'ol/View'
+import proj4 from 'proj4'
+import { defaults } from 'ol/interaction'
+import { register } from 'ol/proj/proj4'
+
 import { addClearButton } from './controls'
 import { addDrawInteraction } from './interactions'
 import { createBaseLayer, createVectorLayer } from './layers.js'
+
+proj4.defs(
+  'EPSG:21781',
+  `+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1
+  +x_0=600000 +y_0=200000 +ellps=bessel
+  +towgs84=660.077,13.551,369.344,2.484,1.783,2.939,5.66 +units=m +no_defs`
+)
+register(proj4)
 
 const format = new GeoJSONFormat()
 const maps = []
