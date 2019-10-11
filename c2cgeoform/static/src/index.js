e9fe162d87a69db76dc937082bd992b0d862daba
Antoine Abt <antoine@abt.im>
2019-10-11 13:45:37 +0200

    feat: allow to register projection

--- c2cgeoform/static/src/index.js
+++ c2cgeoform/static/src/index.js
@@ -11,12 +11,6 @@ import { addClearButton } from './controls'
 import { addDrawInteraction } from './interactions'
 import { createBaseLayer, createVectorLayer } from './layers.js'
 
-proj4.defs(
-  'EPSG:21781',
-  `+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1
-  +x_0=600000 +y_0=200000 +ellps=bessel
-  +towgs84=660.077,13.551,369.344,2.484,1.783,2.939,5.66 +units=m +no_defs`
-)
 register(proj4)
 
 const format = new GeoJSONFormat()
@@ -52,4 +46,8 @@ export function checkInitialized(oid) {
   const initialized = widgets.includes(oid)
   widgets.push(oid)
   return initialized
+}
+
+export function registerProjection(epsg, def) {
+  proj4.defs(epsg, def)
 }
