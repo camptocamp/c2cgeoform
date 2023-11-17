8f1224071a2c016a092d0caefb3d27c927248a26
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-11-17 14:49:37 +0100

    Uses newer pip version

--- c2cgeoform_demo_dev.mk
+++ c2cgeoform_demo_dev.mk
@@ -1,6 +1,7 @@
 include Makefile
 
 .build/requirements.timestamp: .build/venv.timestamp requirements.txt
+	$(VENV_BIN)/pip install pip==22.0.2
 	$(VENV_BIN)/pip install -r requirements.txt -e .
 	$(VENV_BIN)/pip install -e ../..
 	touch $@
